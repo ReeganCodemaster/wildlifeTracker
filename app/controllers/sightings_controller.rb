009e7c492f3d73e6661acce85f5bc0a55f89f7cd
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-04 22:37:55 +0200

    Added create method in sightings cont

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,12 @@
 class SightingsController < ApplicationController
+    def create
+        @animal = Animal.find(params[:animal_id])
+        @sighting = @animal.sightings.create(sightings_params)
+        redirect_to animal_path(@animal)
+    end
+
+    private
+    def sightings_params()
+        params.require(:sighting).permit(:animal, :date, :longitude, :latitude)
+    end
 end
