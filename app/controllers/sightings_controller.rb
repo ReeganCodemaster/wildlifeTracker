77c5fa6c0ef0f9ecb7cc9f4c00652ba1e9c88c8d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-05 16:09:30 +0200

    added edit, update methods to sightings controller

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,6 +4,23 @@ class SightingsController < ApplicationController
         @sighting = @animal.sightings.create(sightings_params)
         redirect_to animal_path(@animal)
     end
+    def edit
+        @animal = Animal.find(params[:animal_id])
+        @sighting = @animal.sightings.find(params[:id])
+    end
+    def update
+        # if save works -> redirect the animal page
+        # if save fails -> redirect the edit form
+        @animal = Animal.find(params[:animal_id])
+        @sighting = @animal.sightings.find(params[:id])
+
+        if @sighting.update(sightings_params)
+            redirect_to @animal
+        else 
+            redirect_to edit_animal_sighting_path
+        end
+    end
+
     def destroy
         @animal = Animal.find(params[:animal_id])
         @sighting = @animal.sightings.find(params[:id])
