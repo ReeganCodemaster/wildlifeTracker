1cb98219af794c915199278d7e659f8ac355196e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-05 22:16:18 +0200

    added get report route for sightings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "animals#index"
   resources :animals do
-    resources :sightings
+    resources :sightings do
+      get 'report', on: :member
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
