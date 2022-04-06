c8a59008398b0b2b4a88d3df5e13c785c81fb216
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 10:24:01 +0200

    got rid of repoerts route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "animals#index"
   resources :animals do
-    resources :sightings do
-      get 'report', on: :member
-    end
+    resources :sightings
   end
   # Defines the root path route ("/")
   # root "articles#index"
