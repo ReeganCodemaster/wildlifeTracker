e7b302700551ac309a945e09fea0bd259723a598
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 14:33:10 +0200

    made sightings belong_to regions

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,5 +1,6 @@
 class Sighting < ApplicationRecord
   belongs_to :animal
+  belongs_to :region
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates :date, presence: true
