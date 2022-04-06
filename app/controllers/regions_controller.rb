bfc324dd5c5c001e5af99eacc4b9c09ecf02b1ba
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 11:39:48 +0200

    added create, new methods to regions controller

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -6,4 +6,23 @@ class RegionsController < ApplicationController
     def show
         @region = Region.find(params[:id])
     end
+
+    def new
+        @region = Region.new()
+    end
+
+    def create
+        @region = Region.new(region_params)
+        
+        if region.save
+            redirect_to @region
+        else
+            redirect_to :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def region_params
+        params.require(:region).permit(:region)
+    end
 end
