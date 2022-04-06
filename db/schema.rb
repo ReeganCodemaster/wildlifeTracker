c9f4f09a4cf555334ef876f495826f2550529543
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 10:27:24 +0200

    Ran region migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_202321) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_082547) do
   create_table "animals", force: :cascade do |t|
     t.string "animal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.text "region"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
