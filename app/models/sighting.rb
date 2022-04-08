7c238159dc69c004c998f5b8eea48db8a772fb34
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 11:13:30 +0200

    added comparison validations to latitude,longitude

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,8 +1,9 @@
 class Sighting < ApplicationRecord
   belongs_to :animal
   belongs_to :region
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+
+  validates :latitude, presence: true , comparison: {greater_than: 0}
+  validates :longitude, presence: true , comparison: {greater_than: 0}
   validates :date, presence: true
 
 end
