0c2bc99fc86df8e308c729ec243a14ac35d083e6
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 11:09:23 +0200

    added length validation to region

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,4 @@
 class Region < ApplicationRecord
     has_many :sightings, dependent: :destroy
-    validates :region, presence: true
+    validates :region, presence: true, length: {minimum: 5}
 end
