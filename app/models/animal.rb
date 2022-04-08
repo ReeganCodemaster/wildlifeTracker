555a70f85f48004861d4c31b90a5647049845a55
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 11:08:27 +0200

    added length validation to animal feild

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,4 @@
 class Animal < ApplicationRecord
     has_many :sightings, dependent: :destroy
-    validates :animal , presence: true
+    validates :animal , presence: true, length: {minimum: 5}
 end
