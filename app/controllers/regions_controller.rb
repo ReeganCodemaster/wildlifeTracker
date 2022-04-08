f54bc4350a59b4d3a3348743ae426b98f51f2835
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:10:26 +0200

    refactored tab spaces

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,45 +1,45 @@
 class RegionsController < ApplicationController
-    before_action :set_region , only: %i[show edit update destroy]
+  before_action :set_region , only: %i[show edit update destroy]
     
-    def index
-      @regions = Region.all()
-    end
+  def index
+    @regions = Region.all()
+  end
 
-    def new
-      @region = Region.new()
-    end
+  def new
+    @region = Region.new()
+  end
 
-    def create
-      @region = Region.new(region_params)
-        
-      if @region.save
-        redirect_to @region
-      else
-        render :new, status: :unprocessable_entity
-      end
+  def create
+    @region = Region.new(region_params)
+       
+    if @region.save
+      redirect_to @region
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def update
-      if @region.update(region_params)
-        redirect_to @region
-      else 
-        render :edit , status: :unprocessable_entity 
-      end
+  def update
+    if @region.update(region_params)
+      redirect_to @region
+    else 
+      render :edit , status: :unprocessable_entity 
     end
+  end
     
-    def destroy
-      @region.destroy
+  def destroy
+    @region.destroy
 
-      redirect_to "/regions", status: 303
-    end
+    redirect_to "/regions", status: 303
+  end
     
-    private
+  private
 
-    def set_region
-      @region = Region.find(params[:id])
-    end
+  def set_region
+    @region = Region.find(params[:id])
+  end
 
-    def region_params
-      params.require(:region).permit(:region)
-    end
+  def region_params
+    params.require(:region).permit(:region)
+  end
 end
