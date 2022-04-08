1420dc7d485881716e4fbedf25da392e9cda86c1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:04:24 +0200

    refactored tab spaces

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -2,44 +2,44 @@ class RegionsController < ApplicationController
     before_action :set_region , only: %i[show edit update destroy]
     
     def index
-        @regions = Region.all()
+      @regions = Region.all()
     end
 
     def new
-        @region = Region.new()
+      @region = Region.new()
     end
 
     def create
-        @region = Region.new(region_params)
+      @region = Region.new(region_params)
         
-        if @region.save
-            redirect_to @region
-        else
-            render :new, status: :unprocessable_entity
-        end
+      if @region.save
+        redirect_to @region
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
 
     def update
-        if @region.update(region_params)
-            redirect_to @region
-        else 
-            render :edit , status: :unprocessable_entity 
-        end
+      if @region.update(region_params)
+        redirect_to @region
+      else 
+        render :edit , status: :unprocessable_entity 
+      end
     end
     
     def destroy
-        @region.destroy
+      @region.destroy
 
-        redirect_to "/regions", status: 303
+      redirect_to "/regions", status: 303
     end
     
     private
 
     def set_region
-        @region = Region.find(params[:id])
+      @region = Region.find(params[:id])
     end
 
     def region_params
-        params.require(:region).permit(:region)
+      params.require(:region).permit(:region)
     end
 end
