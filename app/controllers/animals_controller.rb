600936b3a1e7cb6e94de015c9f653e5c098c2005
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:12:11 +0200

    refactored tab spacing

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,45 +1,45 @@
 class AnimalsController < ApplicationController
-    before_action :set_animal , only: %i[show edit update destroy]
+  before_action :set_animal , only: %i[show edit update destroy]
 
-    def index
-      @animals = Animal.all
+  def index
+    @animals = Animal.all
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to @animal
+    else
+      render :new , status: :unprocessable_entity
     end
+  end
 
-    def new
-      @animal = Animal.new
+  def update
+    if @animal.update(animal_params)
+      redirect_to @animal
+    else 
+      render :edit , status: :unprocessable_entity
     end
+  end
 
-    def create
-      @animal = Animal.new(animal_params)
+  def destroy
+    @animal.destroy
 
-      if @animal.save
-        redirect_to @animal
-      else
-        render :new , status: :unprocessable_entity
-      end
-    end
+    redirect_to root_path , status: :see_other
+  end
 
-    def update
-      if @animal.update(animal_params)
-        redirect_to @animal
-      else 
-        render :edit , status: :unprocessable_entity
-      end
-    end
+  private
 
-    def destroy
-      @animal.destroy
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
 
-      redirect_to root_path , status: :see_other
-    end
-
-    private
-
-    def set_animal
-      @animal = Animal.find(params[:id])
-    end
-
-    def animal_params()
-      params.require(:animal).permit(:animal)
-    end
+  def animal_params()
+    params.require(:animal).permit(:animal)
+  end
 end
