7ca2b014517f141ba026b030a1b039171637376c
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:36:32 +0200

    refactored tab spacing

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,4 @@
 class Animal < ApplicationRecord
-    has_many :sightings, dependent: :destroy
-    validates :animal , presence: true
+  has_many :sightings, dependent: :destroy
+  validates :animal , presence: true
 end
