135786315e9487d42e5dafa38fcab076684469da
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:02:36 +0200

    ractored tab spaces

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,45 +2,44 @@ class AnimalsController < ApplicationController
     before_action :set_animal , only: %i[show edit update destroy]
 
     def index
-        @animals = Animal.all
+      @animals = Animal.all
     end
 
-
     def new
-        @animal = Animal.new
+      @animal = Animal.new
     end
 
     def create
-        @animal = Animal.new(animal_params)
+      @animal = Animal.new(animal_params)
 
-        if @animal.save
-            redirect_to @animal
-        else
-            render :new , status: :unprocessable_entity
-        end
+      if @animal.save
+        redirect_to @animal
+      else
+        render :new , status: :unprocessable_entity
+      end
     end
 
     def update
-        if @animal.update(animal_params)
-            redirect_to @animal
-        else 
-            render :edit , status: :unprocessable_entity
-        end
+      if @animal.update(animal_params)
+        redirect_to @animal
+      else 
+        render :edit , status: :unprocessable_entity
+      end
     end
 
     def destroy
-        @animal.destroy
+      @animal.destroy
 
-        redirect_to root_path , status: :see_other
+      redirect_to root_path , status: :see_other
     end
 
     private
 
     def set_animal
-        @animal = Animal.find(params[:id])
+      @animal = Animal.find(params[:id])
     end
 
     def animal_params()
-        params.require(:animal).permit(:animal)
+      params.require(:animal).permit(:animal)
     end
 end
