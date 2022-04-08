c08c9ef6ea6b741438e384f1f0d352f25d9d3cc1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-08 13:45:15 +0200

    removed unnescarry validation

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -2,7 +2,7 @@ class Sighting < ApplicationRecord
   belongs_to :animal
   belongs_to :region
 
-  validates :latitude, presence: true , comparison: {greater_than: 0}
-  validates :longitude, presence: true , comparison: {greater_than: 0}
+  validates :latitude, presence: true
+  validates :longitude, presence: true
   validates :date, presence: true
 end
